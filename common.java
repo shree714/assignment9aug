c55b6fca731f2d928a9e116ab73693f307d86fe2
shree714 <141352167+shree714@users.noreply.github.com>
2023-08-09 16:50:19 +0530

    Update common.java

--- common.java
+++ common.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloDev1Change, World!"); 
+        System.out.println("HelloDeveleoper2Change, World!"); 
     }
 }
